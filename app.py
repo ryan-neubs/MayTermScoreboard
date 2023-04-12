b6e0bd4c6e2066dac78133477c12e8c56fa8f1c3
Ryan Neubauer <rjneub09@gmail.com>
2023-04-12 14:34:23 -0500

    Scuffed version of scoreboard

--- app.py
+++ app.py
@@ -1,28 +1,53 @@
 # app.py
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 import json
 from pprint import pprint as pretty
 from dotenv import load_dotenv
 import os
+from time import sleep
 
 app = Flask(__name__)
-@app.route('/points', methods=['GET', 'POST'])
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        form = request.form
-        if verifypw(form["passkey"]):
-            update_points(form["class"], form["points"])
+
     data = get_points()
     freshmen = data["Points"]["Freshmen"]
     sophomore = data["Points"]["Sophomore"]
     junior = data["Points"]["Junior"]
     senior = data["Points"]["Senior"]
+
     return render_template('points.html',
                            freshmen=freshmen, 
                            sophomore=sophomore,
                            junior=junior,
                            senior=senior)
+
+@app.route('/pointedit', methods=['GET', 'POST'])
+def pointedit():
+    if request.method == 'POST':
+        form = request.form
+        if verifypw(form["passkey"]):
+            update_points(form["class"], form["points"])
+    return render_template('editor.html')
+
+@app.route('/reset', methods=['GET', 'POST'])
+def reset():
+    if request.method == 'POST':
+         form = request.form
+         if verifypw(form["passkey"]):
+             reset_points()
+    return redirect('/')
+
+def reset_points():
+    data = get_points()
+    for p in data["Points"]:
+        data["Points"][p] = 0
+    with open('points.json', 'w') as data_file:
+        json.dump(data, data_file, indent=4, sort_keys=True)
 
 def update_points(year, amount):
     amount = int(amount)
@@ -36,12 +61,6 @@ def get_points():
         data = json.load(data_file)
     return data
 
-def reset_points():
-    data = get_points()
-    for p in data["Points"]:
-        data["Points"][p] = 0
-    with open('points.json', 'w') as data_file:
-        json.dump(data, data_file, indent=4, sort_keys=True)
 
 # For whoever read this: The passkey will be stored in a .env file. It is in the gitignore template so
 # it isn't out on the repository for anyone to see. You can make the passkey whatever you want in the .env
